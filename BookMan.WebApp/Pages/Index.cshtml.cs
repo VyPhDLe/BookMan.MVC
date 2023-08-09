3495df7b058a17c7d1039f5467cf2bc2348481ce
Vy Le <vyphd.le@gmail.com>
2023-08-09 14:10:13 -0500

    Add project files.

---
+++ BookMan.WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using BookMan.WebApp.Interface;
+using BookMan.WebApp.Model;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace BookMan.WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IRepository _repository;
+        public HashSet<Book> Books => _repository.Books;
+        public int Count => _repository.Books.Count;
+        public IndexModel(IRepository repository) => _repository = repository;
+    }
+}
